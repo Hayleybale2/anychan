3614d11d22891c0cffe7211240efa2277b0bec23
Hayleybale2 <hayleybale2@gmail.com>
2025-10-16 01:09:39 -0400

    Create crbachan-with-resources.ts

---
+++ dataSources/imageboards/crbachan-with-resources.ts
@@ -0,0 +1,6 @@
+import type { ImageboardDataSourceDefinitionWithResources } from '@/types'
+
+import Properties from './crbachan.js'
+
+import Icon from './crbachan-icon.png'
+import Logo from './crbachan-icon.png'
